c3d5ea0415ee2907235d67d7dff4757ca9c5f08c
akshitaladdha27 <akshita.laddha27@gmail.com>
2026-02-19 20:36:09 +0530

    Adding trivy-scan.yml

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
 FROM python:3.12-slim
 
-RUN apt-get update && apt-get install -y curl
+WORKDIR /second
 
-WORKDIR /usr/local/app
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY app.py .
+
+EXPOSE 5000
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Hello from the Dockerized Python application!")
+import requests
+
+def main():
+    print("Fetching data from external API...")
+
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+
+    if response.status_code == 200:
+        print("Success!")
+        print("Response:", response.json())
+    else:
+        print("Failed with status:", response.status_code)
+
+if __name__ == "__main__":
+    main()
