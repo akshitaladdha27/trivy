0de946db34a1013bc6b21c88b7b1c5d220728d85
Akshita Laddha <117356670+akshitaladdha27@users.noreply.github.com>
2026-04-29 17:23:34 +0530

    Rename variable 'passwords' to 'password'

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-passwords = "AKIAIOSFODNN7EXAMPLE"
+password = "AKIAIOSFODNN7EXAMPLE"
 api_key = "ghp_abcd1234abcd1234abcd1234abcd1234abcd"
 def home():
     return "Hello World"
