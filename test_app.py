1feb33d0dee7c021f9f716a3e5dc9ef33dc0194e
Akshita Laddha <117356670+akshitaladdha27@users.noreply.github.com>
2026-04-29 18:45:17 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 password = "AKIAIOSFODNN7EXAMPLE"
 api_key = "ghp_abcd1234abcd1234abcd1234abcd1234abcd"
+aws_key = "AKIA1234567890ABCD12"
 def home():
     return "Hello World"
